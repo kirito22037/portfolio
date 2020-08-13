fd5e6e33d3f0ade5a1194946e47f5aeb582a606d
kirito22037 <kirito22037@gmail.com>
2020-08-13 18:01:35 +0530

    started creating portfolio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Home = ()=>{
+
+    return <div 
+        id="home-p"
+        className="f-wrapper full-p">
+
+            <div>
+            <div className="branding">
+                <h1 className="minimal-h1">Hello! <br/> I am Roshan Rai</h1>
+                <h2 className="minimal-h2">Software Development Engineer</h2>
+            </div>
+            <div className="contact-link">
+                <i class="fab fa-facebook-square"></i>
+                <i class="fab fa-instagram"></i>
+                <i class="fab fa-linkedin"></i>
+                <i class="fab fa-whatsapp-square"></i>
+            </div>
+            </div>
+    </div>
+};
+
+export default Home;
