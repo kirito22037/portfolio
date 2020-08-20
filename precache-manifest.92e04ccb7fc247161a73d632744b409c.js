27bb970c1bc044960cc9d56b8d4471e93f312138
kirito22037 <kirito22037@gmail.com>
2020-08-20 15:57:28 +0530

    Updates

---
+++ precache-manifest.92e04ccb7fc247161a73d632744b409c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca9f989639d9cfc3301d04fd4c77258c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "dcedd0c912bca12bee90",
+    "url": "/portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a0456a37c0ed5663377b",
+    "url": "/portfolio/static/css/main.401c00a5.chunk.css"
+  },
+  {
+    "revision": "dcedd0c912bca12bee90",
+    "url": "/portfolio/static/js/2.9166a74e.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/portfolio/static/js/2.9166a74e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0456a37c0ed5663377b",
+    "url": "/portfolio/static/js/main.3ee87599.chunk.js"
+  },
+  {
+    "revision": "bc9113cef4c532fcef22",
+    "url": "/portfolio/static/js/runtime-main.8977ed00.js"
+  },
+  {
+    "revision": "fed8aba0c1956612213526ce3e9109ef",
+    "url": "/portfolio/static/media/ConnectionIi-2wj8.fed8aba0.otf"
+  }
+]);
